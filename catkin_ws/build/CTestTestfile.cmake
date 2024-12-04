107fcf9df5cac8cc8fff94a64fbd7c35390269fe
Tao Xiang <xiang.tao@outlook.de>
2024-12-04 17:00:16 +0800

    1

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/leo/git/hikvision_ros_driver/catkin_ws/src
-# Build directory: /home/leo/git/hikvision_ros_driver/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("hikvision_ros_driver")
--- catkin_ws/build/hikvision_ros_driver/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "hikvision_ros_driver"
-PROJECT_SPACE_DIR = "/home/leo/git/hikvision_ros_driver/catkin_ws/install"
-PROJECT_VERSION = "0.0.0"
